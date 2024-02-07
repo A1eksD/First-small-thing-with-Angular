62135c1d919c226ecb17fccf97b2e79d6686e985
Aleks <143895611+A1eksD@users.noreply.github.com>
2024-02-07 13:56:47 +0100

    added some style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './main-content/loading-page/landingPage.component';
 import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent, MainContentComponent],
+  imports: [RouterOutlet, LandingPageComponent, MainContentComponent, FooterComponent, ImprintComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { LandingPageComponent } from './loading-page/landingPage.component';
 import { OurManuComponent } from './our-manu/our-manu.component';
+import { HowToOrderComponent } from './how-to-order/how-to-order.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [ CommonModule, LandingPageComponent, OurManuComponent],
+  imports: [ CommonModule, LandingPageComponent, OurManuComponent, HowToOrderComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
