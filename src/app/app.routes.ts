c1fa31d1f27b0ea049b11d02d5dd5a85d41d74d7
Aleks <143895611+A1eksD@users.noreply.github.com>
2024-02-08 18:30:18 +0100

    added variables in scss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { MainContentComponent } from './main-content/main-content.component';
 import { ImprintComponent } from './imprint/imprint.component';
 
 export const routes: Routes = [ //lade eine neue seite(optisch) ohne den browser zu aktuallesieren
-    { path: 'navbar', component: MainContentComponent }, //lade standart seite
-    { path: 'imprint', component: ImprintComponent }, // bein anklicken -lade neue seite
+{ path: '', component: MainContentComponent }, //lade standart seite
+{ path: 'header', component: MainContentComponent }, //lade standart seite
+{ path: 'imprint', component: ImprintComponent } // bein anklicken -lade neue seite
     // { path: 'second-component', component: SecondComponent },
 ];
